56f1e76d5f5df0ab7b1b024c08eabfe65c535f61
53gur0 <278466100+53gur0@users.noreply.github.com>
2026-04-30 01:18:19 +0200

    feat: generalized components for ENS resolution

--- suite-common/test-utils/src/index.ts
+++ suite-common/test-utils/src/index.ts
@@ -3,5 +3,6 @@ export * from './configureMockStore';
 export * from './extraDependenciesCommonMock';
 export * from './wireEnabledNetworksMock';
 export { renderHookWithStoreProvider, type TestStore } from './renderWithStore';
+export { renderHookWithQueryClient, newTestQueryClient } from './renderWithQueryClient';
 
 export * from '@testing-library/react';
